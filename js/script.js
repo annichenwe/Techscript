272d06a8ae12f03971710465eb5aea6219fed471
annichenwe <annichenwe@gmail.com>
2017-09-12 14:30:06 +0200

    Reading time main page
    NOT WORKING……!!!!!

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(function () {
 
         $(this).readingTime({
             readingTimeTarget: $(this).find('.eta'),
-            wordCountTarget: $(this).find('.words'),
             remotePath: $(this).attr('data-file'),
             remoteTarget: $(this).attr('data-target')
         });
